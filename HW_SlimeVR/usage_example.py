92235b88dd4ef9c9d746e6db042bc262ddaea139
Alex <alex.crownshaw12@gmail.com>
2024-02-08 16:43:51 +0000

    Performance optimisation using blocking UDP server instead of threading server

--- HW_SlimeVR/usage_example.py
+++ HW_SlimeVR/usage_example.py
@@ -1,3 +1,5 @@
+import time
+
 from HW_SlimeVR.hw_tracking import HWTracking
 
 
@@ -5,13 +7,19 @@ UDP_ADDR = "127.0.0.1"
 UDP_PORT = 9000
 TRACKING_POINTS = ["l_ankle", "l_thigh", "r_ankle", "r_thigh", "hip"]
 
+INTERVAL = 0.01
+
 
 def main():
     hw = HWTracking(addr=UDP_ADDR, port=UDP_PORT, track_points=TRACKING_POINTS)   # Create HWTracking object
 
+    t_last = 0
     while 1:
-        l_ankle_position = hw.get_position_data(track_point="l_ankle")  # Get current positional data for a tracking point
-        print(f"Left Ankle:\nx [m]: {l_ankle_position[0]}\ny [m]: {l_ankle_position[1]}\nz [m]: {l_ankle_position[2]}\n")
+        t_current = time.time()
+        if t_current - t_last > INTERVAL:
+            l_ankle_position = hw.get_position_data(track_point="l_ankle")  # Get current positional data for a tracking point
+            print(f"Left Ankle:\nx [m]: {l_ankle_position[0]}\ny [m]: {l_ankle_position[1]}\nz [m]: {l_ankle_position[2]}\n")
+            t_last = t_current
 
 
 if __name__ == "__main__":
